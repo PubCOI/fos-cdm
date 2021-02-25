069fd40f63297cd74c2c3e154005ff557a448481
Ronald MacDonald <ronald@rmacd.com>
2021-02-25 21:54:20 +0000

    initial commit

---
+++ src/main/java/org/pubcoi/fos/cdm/AttachmentFactory.java
@@ -0,0 +1,23 @@
+package org.pubcoi.fos.cdm;
+
+import org.pubcoi.fos.cdm.attachments.Attachment;
+import org.pubcoi.fos.models.cf.AdditionalDetailsType;
+
+public class AttachmentFactory {
+
+    public AttachmentFactory() {}
+
+    public static Attachment build(AdditionalDetailsType additionalDetailsType) {
+
+        return new Attachment()
+                .withId(additionalDetailsType.getId())
+                .withNoticeId(additionalDetailsType.getNoticeId())
+                .withDescription(additionalDetailsType.getDescription())
+                .withDataType(additionalDetailsType.getDataType())
+                .withLink(additionalDetailsType.getLink())
+                .withTextData(additionalDetailsType.getTextData())
+                .withMimeType(additionalDetailsType.getMimeType())
+                .withAwardGuid(additionalDetailsType.getAwardGuid());
+    }
+
+}
