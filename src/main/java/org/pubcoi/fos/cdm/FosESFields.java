a44469df5654685cba1c4a9c36dda9d94769964f
Ronald MacDonald <ronald@rmacd.com>
2021-03-08 01:07:08 +0000

    ignore s3LocationType/created in equals() and hashCode()

---
+++ src/main/java/org/pubcoi/fos/cdm/FosESFields.java
@@ -0,0 +1,14 @@
+package org.pubcoi.fos.cdm;
+
+public class FosESFields {
+
+    public static final String TYPE = "type";
+    public static final String FILE_SIZE = "file_size";
+    public static final String DATA = "data";
+    public static final String SHA1 = "file_sha1";
+    public static final String ATTACHMENT_ID = "attachment_id";
+    public static final String NOTICE_ID = "notice_id";
+    public static final String PAGE_NUMBER = "page";
+    public static final String DOC_TYPE = "fos_document_type";
+
+}
