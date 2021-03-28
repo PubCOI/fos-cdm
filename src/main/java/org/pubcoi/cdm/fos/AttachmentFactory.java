6d13358990179264055bd8d337424afce23ebb75
Ronald MacDonald <ronald@rmacd.com>
2021-03-28 12:21:05 +0100

    add license ; add enforcer rules

---
+++ src/main/java/org/pubcoi/cdm/fos/AttachmentFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 PubCOI.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.pubcoi.cdm.fos;
+
+import org.pubcoi.cdm.cf.AdditionalDetailsType;
+import org.pubcoi.cdm.cf.attachments.Attachment;
+
+public class AttachmentFactory {
+
+    private AttachmentFactory() {}
+
+    public static Attachment build(AdditionalDetailsType additionalDetailsType) {
+
+        return new Attachment()
+                .withId(additionalDetailsType.getId())
+                .withNoticeId(additionalDetailsType.getNoticeId())
+                .withDescription(additionalDetailsType.getDescription())
+                .withDataType(additionalDetailsType.getDataType())
+                .withLink(additionalDetailsType.getLink())
+                .withTextData(additionalDetailsType.getTextData())
+                .withMimeType(additionalDetailsType.getMimeType())
+                .withAwardGuid(additionalDetailsType.getAwardGuid());
+    }
+
+}
---
+++ src/main/java/org/pubcoi/cdm/fos/FosESFields.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 PubCOI.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.pubcoi.cdm.fos;
+
+public class FosESFields {
+
+    public static final String TYPE = "type";
+    public static final String FILE_SIZE = "file_size";
+    public static final String DATA = "data";
+    public static final String SHA1 = "file_sha1";
+    public static final String ATTACHMENT_ID = "attachment_id";
+    public static final String NOTICE_ID = "notice_id";
+    public static final String PAGE_NUMBER = "page";
+    public static final String DOC_TYPE = "fos_document_type";
+
+}
